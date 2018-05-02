40888d60e589713ecd91ff18654c212b68332553
Dan Mosora <dan@stitchdata.com>
2018-05-02 16:22:56 -0400

    Update tracking comment

--- tap_zendesk/discover.py
+++ tap_zendesk/discover.py
@@ -7,15 +7,12 @@ def discover_streams(client):
         streams.append({'stream': s.name, 'tap_stream_id': s.name, 'schema': s.load_schema(), 'metadata': s.load_metadata()})
     return streams
 
-# * Audits (P1)
-
 # * Ticket Comments (P2)
 # * Others from Audit (P2)
 
 # P2?
 # Use "search" Endpoint; for things like ticket_metrics
 #   * zenpy_client.search("some query", type='ticket_metrics', sort_by='created_at', sort_order='desc')
-
 
 # P2?
 # "Relationships" (?)
