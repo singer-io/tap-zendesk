283e3b9cdbf493b005e6c367d4865d3f0549c096
Nick McCoy <nick@stitchdata.com>
2018-08-15 15:42:56 +0000

    remove load_shared_schema_refs from streams.py

--- tap_zendesk/streams.py
+++ tap_zendesk/streams.py
@@ -65,18 +65,6 @@ class Stream():
         if value and utils.strptime_with_tz(value) > current_bookmark:
             singer.write_bookmark(state, self.name, self.replication_key, value)
 
-    def load_shared_schema_refs(self):
-        shared_schemas_path = get_abs_path('schemas/shared')
-
-        shared_file_names = [f for f in os.listdir(shared_schemas_path)
-                             if os.path.isfile(os.path.join(shared_schemas_path, f))]
-
-        shared_schema_refs = {}
-        for shared_file in shared_file_names:
-            with open(os.path.join(shared_schemas_path, shared_file)) as data_file:
-                shared_schema_refs[shared_file] = json.load(data_file)
-
-        return shared_schema_refs
 
     def load_schema(self):
         schema_file = "schemas/{}.json".format(self.name)
