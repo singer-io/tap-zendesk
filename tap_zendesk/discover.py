06d4ff7a9540350510704fbf2cdfc614ed3861a3
Nick McCoy <nick@stitchdata.com>
2018-08-15 16:27:59 +0000

    pylint fix

--- tap_zendesk/discover.py
+++ tap_zendesk/discover.py
@@ -1,7 +1,7 @@
 import os
 import json
-from tap_zendesk.streams import STREAMS
 import singer
+from tap_zendesk.streams import STREAMS
 
 def get_abs_path(path):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
