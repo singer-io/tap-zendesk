46843c38b10d0d8e866172cd2d967ed4410c3dfa
Tim Visher <tim@stitchdata.com>
2019-05-03 16:53:41 +0000

    pylint

--- tap_zendesk/metrics.py
+++ tap_zendesk/metrics.py
@@ -1,4 +1,3 @@
-import pytz
 import logging
 from statistics import mean
 from collections import defaultdict
@@ -28,7 +27,7 @@ def _seconds_since_datetime(dt):
     "Returns the number of seconds since DT"
     return (datetime.utcnow() - dt).seconds
 
-def _log_aggregate_rates(capture_rate, aggregate_rates):
+def _log_aggregate_rates(current_capture_rate, aggregate_rates):
     """Logs the aggregate rates"""
     if not list(aggregate_rates.items()):
         LOGGER.info("No zendesk metrics were captured")
@@ -37,52 +36,52 @@ def _log_aggregate_rates(capture_rate, aggregate_rates):
             LOGGER.info("Synced average of %s %ss per %s seconds",
                         mean(value),
                         metric,
-                        capture_rate)
+                        current_capture_rate)
             LOGGER.info("Synced minimum of %s %ss per %s seconds",
                         min(value),
                         metric,
-                        capture_rate)
+                        current_capture_rate)
             LOGGER.info("Synced max of %s %ss per %s seconds",
                         max(value),
                         metric,
-                        capture_rate)
+                        current_capture_rate)
             LOGGER.info("Synced total of %s %ss in %s seconds",
                         sum(value),
                         metric,
                         # Slightly idealized view of how long we've been
                         # capturing metrics for.
-                        capture_rate * len(value))
+                        current_capture_rate * len(value))
 
 
-def _aggregate_rates(capture_rate, metrics_data):
+def _aggregate_rates(current_capture_rate, current_metrics_data):
     """Captures the aggregate rates, resets the capture window, and logs the
     new aggregate rates"""
     LOGGER.debug(
         "Computing aggregate metrics over the previous %d seconds",
-        capture_rate)
-    window_counts = metrics_data['window_counts']
-    aggregate_rates = metrics_data['aggregate_rates']
+        current_capture_rate)
+    window_counts = current_metrics_data['window_counts']
+    aggregate_rates = current_metrics_data['aggregate_rates']
     for metric in list(window_counts.keys()):
         aggregate_rates[metric] += [window_counts.pop(metric)]
-    metrics_data['window_start_time'] = datetime.utcnow()
-    _log_aggregate_rates(capture_rate, metrics_data['aggregate_rates'])
+    current_metrics_data['window_start_time'] = datetime.utcnow()
+    _log_aggregate_rates(current_capture_rate, current_metrics_data['aggregate_rates'])
 
-def _maybe_aggregate_rates(capture_rate, metrics_data):
+def _maybe_aggregate_rates(current_capture_rate, current_metrics_data):
     """Takes metrics_data and aggregates it into the current aggregated rates
     if enough time has passed. If an aggregation happens it the new
     aggregate values are logged.
 
     """
-    if capture_rate <= _seconds_since_datetime(
-            metrics_data['window_start_time']):
-        _aggregate_rates(capture_rate, metrics_data)
+    if current_capture_rate <= _seconds_since_datetime(
+            current_metrics_data['window_start_time']):
+        _aggregate_rates(current_capture_rate, current_metrics_data)
 
-def _capture_raw(metrics_data, metric):
+def _capture_raw(current_metrics_data, metric):
     """Adds one to METRIC in WINDOW_COUNTS defaultdict"""
-    metrics_data['window_counts'][metric] += 1
+    current_metrics_data['window_counts'][metric] += 1
     LOGGER.debug('Raw count for metric %s is %d',
                  metric,
-                 metrics_data['window_counts'][metric])
+                 current_metrics_data['window_counts'][metric])
 
 def capture(metric):
     # Start the metrics window timer if this is the first time capture has
