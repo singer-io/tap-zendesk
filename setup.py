6a6e9ecb4581636c000e95500d2c7dbb5c458b5f
k3llymariee <kellymhofmann@gmail.com>
2020-09-08 17:16:48 -0700

    Bump version to v1.4.25

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-zendesk',
-      version='1.4.24',
+      version='1.4.25',
       description='Singer.io tap for extracting data from the Zendesk API',
       author='Stitch',
       url='https://singer.io',
