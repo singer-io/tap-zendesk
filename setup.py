5ddba52e3880c02066c38bea898bc4abb90c8a85
Jacob Baca <jbaca@stitchdata.com>
2020-09-16 18:37:18 +0000

    bump version to 1.5.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-zendesk',
-      version='1.4.7',
+      version='1.5.0',
       description='Singer.io tap for extracting data from the Zendesk API',
       author='Stitch',
       url='https://singer.io',
