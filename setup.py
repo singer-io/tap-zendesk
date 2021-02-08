875cf213513ad41d91044bd4e6a832fcf6df974e
Benjamin Maquet <bmaquet@twilio.com>
2021-02-08 22:27:59 +0100

    Add field to users schema

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='tap-zendesk',
       py_modules=['tap_zendesk'],
       install_requires=[
           'singer-python==5.2.1',
-          'zenpy==2.0.24',
+          'zenpy==2.0.10',
       ],
       extras_require={
           'dev': [
