8abf29e4e43b026847c6dec1049526f7945636ee
Dan Mosora <30501696+dmosorast@users.noreply.github.com>
2021-09-07 11:09:50 -0400

    v1.5.4: Log request URL, etag, and request ID for debugging purposes (#63)
    * Log request URL, etag, and request ID for debugging purposes
    
    * Verswion 1.5.4 and changelog
    
    * Use safe get on headers. Cases-insensitive does not mean presence-insensitive

--- tap_zendesk/__init__.py
+++ tap_zendesk/__init__.py
@@ -36,7 +36,12 @@ request = Session.request
 
 def request_metrics_patch(self, method, url, **kwargs):
     with singer_metrics.http_request_timer(None):
-        return request(self, method, url, **kwargs)
+        response = request(self, method, url, **kwargs)
+        LOGGER.info("Request: %s, Response ETag: %s, Request Id: %s",
+                    url,
+                    response.headers.get('ETag', 'Not present'),
+                    response.headers.get('X-Request-Id', 'Not present'))
+        return response
 
 Session.request = request_metrics_patch
 # end patch
