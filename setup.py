c7fb9bc3d4616da2a9b7298f86708a457790d9ca
Collin Simon <csimon@talend.com>
2021-09-08 17:38:14 +0000

    update singer-python version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='tap-zendesk',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_zendesk'],
       install_requires=[
-          'singer-python==5.2.1',
+          'singer-python==5.12.2',
           'zenpy==2.0.24',
       ],
       extras_require={
