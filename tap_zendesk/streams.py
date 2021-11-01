6e9b47efee2a55fc0306e769b0ff80412fe19d03
namrata270998 <namrata.brahmbhatt@crestdatasys.com>
2021-11-01 19:03:41 +0530

    added comment in the streams.py

--- tap_zendesk/streams.py
+++ tap_zendesk/streams.py
@@ -139,6 +139,8 @@ def raise_or_log_zenpy_apiexception(schema, stream, e):
         raise ValueError("Called with a bad exception type") from e
 
     error = json.loads(e.args[0]).get('error')
+    # check if the error is of type dictionary and the message retrieved from the dictionary
+    # is the expected message. If so, only then print the logger message and return the schema
     if isinstance(error, dict) and error.get('message', None) == "You do not have access to this page. Please contact the account owner of this help desk for further help.":
         LOGGER.warning("The account credentials supplied do not have access to `%s` custom fields.",
                        stream)
