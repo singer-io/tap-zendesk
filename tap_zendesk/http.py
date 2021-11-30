d853a7f8553be2a0934b17b925f7b74a251c72b1
prijendev <prijen.khokhani@crestdatasys.com>
2021-11-30 14:09:39 +0530

    Added back backoff for all 5xx errors

--- tap_zendesk/http.py
+++ tap_zendesk/http.py
@@ -137,7 +137,7 @@ def raise_for_error(response):
         raise exc(message, response) from None
 
 @backoff.on_exception(backoff.expo,
-                      (HTTPError, ZendeskBackoffError),
+                      (HTTPError, ZendeskError), # Added support of backoff for all 5xx errors.
                       max_tries=10,
                       giveup=is_fatal)
 @backoff.on_exception(backoff.expo,
