singer-io/tap-zendesk

commit 1cb5ebe6e46aae926de05010c97de032488b79f3

Bring in ticket metric events (for sla data)

tap_zendesk/streams.py
@@ -386,6 +386,23 @@ class TicketComments(Stream):
                 child_event['ticket_id'] = event.ticket_id
                 yield (self.stream, child_event)
 
+class TicketMetricEvents(Stream):
+    name = "ticket_metric_events"
+    replication_method = "INCREMENTAL"
+    key_properties = ['id']
+
+    def sync(self, state):
+        bookmark = self.get_bookmark(state)
+        start_time = int(bookmark.timestamp())
+        ticket_metric_events = self.client.ticket_metric_events(start_time=start_time)
+        for event in ticket_metric_events:
+            self.update_bookmark(state, event.time)
+            event_dict = event.to_dict()
+            if event_dict.get('sla'):
+                # TODO I don't know what this consists of yet
+                event_dict['sla'] = json.dumps(event_dict['sla'])
+            yield (self.stream, event)
+
 class SatisfactionRatings(Stream):
     name = "satisfaction_ratings"
     replication_method = "INCREMENTAL"
@@ -601,6 +618,7 @@ STREAMS = {
     "ticket_comments": TicketComments,
     "ticket_fields": TicketFields,
     "ticket_forms": TicketForms,
+    "ticket_metric_events": TicketMetricEvents,
     "group_memberships": GroupMemberships,
     "macros": Macros,
     "satisfaction_ratings": SatisfactionRatings,

.gitignore
@@ -108,3 +108,4 @@ state.*.json
 target-config.json
 target-config.*.json
 tags
+configs/

Makefile
@@ -1,6 +1,30 @@
-.PHONY: test
-.DEFAULT_GOAL := test
+CONF_DIR?=./configs/default
+TARGET_DIR?=../target-postgres
 
+.PHONY: run
+run:
+	tap-zendesk -c "$(CONF_DIR)"/config.json --catalog "$(CONF_DIR)"/catalog.json -p "$(CONF_DIR)"/catalog.json -s "$(CONF_DIR)"/state.json | \
+	"$(TARGET_DIR)"/venv/bin/target-postgres -c "$(CONF_DIR)"/target_config.json
+
+.PHONY: clean_catalog
+clean-catalog:
+	rm "$(CONF_DIR)"/catalog.json
+
+.PHONY: discover
+discover: "$(CONF_DIR)"/catalog.json
+
+"$(CONF_DIR)"/catalog.json: $(CONF_DIR)
+	tap-zendesk -c "$(CONF_DIR)"/config.json -d > "$(CONF_DIR)"/catalog.json
+
+.PHONY: tap
+tap: $(CONF_DIR)
+	tap-zendesk -c "$(CONF_DIR)"/config.json --catalog "$(CONF_DIR)"/catalog.json -p "$(CONF_DIR)"/catalog.json -s "$(CONF_DIR)"/state.json
+
+.PHONY: target
+target: $(CONF_DIR)
+	"$(TARGET_DIR)"/venv/bin/target-postgres -c "$(CONF_DIR)"/target_config.json
+
+.PHONY: test
 test:
 	pylint tap_zendesk -d missing-docstring,invalid-name,line-too-long,too-many-locals,too-few-public-methods,fixme,stop-iteration-return,too-many-branches,useless-import-alias,no-else-return,logging-not-lazy
 	nosetests

setup.py
@@ -14,7 +14,7 @@ setup(name='tap-zendesk',
           # specifically install the correct version of our fork before
           # trying to install this package.
           'singer-python==5.7.*',
-          'zenpy==2.0.12',
+          'zenpy==2.0.24',
       ],
       extras_require={
           'dev': [

commit a7a9aec01133846869f59ed730f94934da354bd2

Fix regression caused by zenpy upgrade
Commit 1cb5ebe6e46aae926de05010c97de032488b79f3 broke our code, as we had a bug workaround in place that was not compatible with the upgrade.

tap_zendesk/streams.py
@@ -350,9 +350,7 @@ class TicketEvents(Stream):
 
         ticket_events = self.client.tickets.events(bookmark, include=include)
         for event in ticket_events:
-            # Don't use event.timestamp here. It tries to parse _timestamp
-            # as a date, but _timestamp is an integer so it fails.
-            event_timestamp = datetime.datetime.fromtimestamp(event._timestamp, pytz.utc)
+            event_timestamp = datetime.datetime.fromtimestamp(event.timestamp, pytz.utc)
             # See the following URL for why we check the timestamp against the bookmark:
             # https://developer.zendesk.com/rest_api/docs/support/incremental_export#excluding-system-updates
             if event_timestamp >= bookmark: