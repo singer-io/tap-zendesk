c05e8d44899dd9619f0d93fa528dbd6c770b483c
Karan Panchal <karan.panchal@crestdatasys.com>
2022-04-05 15:17:52 +0530

    add comment in streams.py and add 2sec window test case

--- test/unittests/test_user_infinite_loop.py
+++ test/unittests/test_user_infinite_loop.py
@@ -37,7 +37,7 @@ class TestUserSyncCheck(unittest.TestCase):
         
     def test_many_records_in_one_seconds_for_user_with_3_sec_window(self):
         """
-            To verify that if user give 3 sec window then also we don't get infinite loop behavior
+            To verify that if user give 3 seconds window then also we don't get infinite loop behavior
         """
         user_obj = Users(client=MockSearch(), config={'search_window_size': 3})
         
@@ -46,4 +46,14 @@ class TestUserSyncCheck(unittest.TestCase):
             
         self.assertEqual(str(e.exception), 'users - Unable to get all users within minimum window of a single second (2022-03-30T08:45:20Z), found 1001 users within this timestamp. Zendesk can only provide a maximum of 1000 users per request. See: https://develop.zendesk.com/hc/en-us/articles/360022563994--BREAKING-New-Search-API-Result-Limits')
         
-    
+    def test_many_records_in_one_seconds_for_user_with_2_sec_window(self):
+        """
+            To verify that if user give 2 seconds window then also we don't get infinite loop behavior
+        """
+        user_obj = Users(client=MockSearch(), config={'search_window_size': 2})
+        
+        with self.assertRaises(Exception) as e:
+            l = list(user_obj.sync({'bookmarks': {'users': {'updated_at': '2022-03-30T08:45:21.000000Z'}}}))
+            
+        self.assertEqual(str(e.exception), 'users - Unable to get all users within minimum window of a single second (2022-03-30T08:45:20Z), found 1001 users within this timestamp. Zendesk can only provide a maximum of 1000 users per request. See: https://develop.zendesk.com/hc/en-us/articles/360022563994--BREAKING-New-Search-API-Result-Limits')
+        
