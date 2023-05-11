6821682174644d7e52bc51b8bd4fe8c1b84cc948
shantanu73 <shantanudhiman25@gmail.com>
2023-05-11 21:47:54 +0000

    Added space

--- tap_zendesk/streams.py
+++ tap_zendesk/streams.py
@@ -239,6 +239,7 @@ class Users(CursorBasedExportStream):
         for user in users:
             self.update_bookmark(state, user["updated_at"])
             yield (self.stream, user)
+
         singer.write_state(state)
 
 
