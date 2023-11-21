f79771fcbc31cc6dc3cc9e41e0560fff124bf993
Leslie VanDeMark <lvandemark@talend.com>
2023-11-21 21:02:07 +0000

    Update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-zendesk',
-      version='2.3.0',
+      version='2.3.1',
       description='Singer.io tap for extracting data from the Zendesk API',
       author='Stitch',
       url='https://singer.io',
