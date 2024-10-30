a1b7b31f7fd119dc5d23e46c80d814845af10892
Prijen Khokhani <88327452+prijendev@users.noreply.github.com>
2024-10-30 14:07:26 +0530

    Add aiohttp module version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='tap-zendesk',
           'zenpy==2.0.24',
           'backoff==2.2.1',
           'requests==2.31.0',
-          'aiohttp'
+          'aiohttp==3.10.10'
       ],
       extras_require={
           'dev': [
