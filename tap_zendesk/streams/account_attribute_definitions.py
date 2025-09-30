dbfa50d80a740ec5c1617db88ef3316933aa6747
atttiwari <atul.tiwari@qlik.com>
2025-09-30 19:17:48 +0530

    SAC-28940: Updated primary key account attributes

--- tap_zendesk/streams/account_attribute_definitions.py
+++ tap_zendesk/streams/account_attribute_definitions.py
@@ -5,6 +5,7 @@ from tap_zendesk.streams.abstracts import (
 class AccountAttributeDefinitions(PaginatedStream):
     name = "account_attribute_definitions"
     replication_method = "FULL_TABLE"
+    key_properties = []
     endpoint = 'routing/attributes/definitions'
     item_key = 'definitions'
     pagination_type = "offset"
