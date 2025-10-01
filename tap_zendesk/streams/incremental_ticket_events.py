5f90dab6083e0009c6ba300819e423a95ba2cb16
atttiwari <atul.tiwari@qlik.com>
2025-10-01 14:03:45 +0530

    SAC-28940: Updated for incremental ticket events pagination

--- tap_zendesk/streams/incremental_ticket_events.py
+++ tap_zendesk/streams/incremental_ticket_events.py
@@ -3,6 +3,7 @@ from tap_zendesk import http
 from tap_zendesk.streams.abstracts import (
     HEADERS,
     START_DATE_FORMAT,
+    DEFAULT_PAGE_SIZE,
     CursorBasedExportStream
 )
 
@@ -23,3 +24,23 @@ class IncrementalTicketEvents(CursorBasedExportStream):
         HEADERS['Authorization'] = 'Bearer {}'.format(self.config["access_token"])
 
         http.call_api(url, self.request_timeout, params={'start_time': start_time, 'per_page': 1}, headers=HEADERS)
+
+    def get_objects(self, start_time, side_load=None):
+        '''
+        Retrieve objects from the incremental exports endpoint using cursor based pagination
+        '''
+        url = self.get_stream_endpoint()
+        seen_ids = set()
+
+        for page in http.get_incremental_export_offset(
+            url,
+            self.config['access_token'],
+            self.request_timeout,
+            DEFAULT_PAGE_SIZE,
+            start_time
+        ):
+            for obj in page[self.item_key]:
+                obj_id = obj.get('id')
+                if obj_id is not None and obj_id not in seen_ids:
+                    seen_ids.add(obj_id)
+                    yield obj
