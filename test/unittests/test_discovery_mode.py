d7d251480fef76ec8ea0d069f03fa71555c52772
satyendra101 <satyendra.singh@qlik.com>
2026-07-28 07:33:14 +0000

    Update unittests

--- test/unittests/test_discovery_mode.py
+++ test/unittests/test_discovery_mode.py
@@ -65,15 +65,14 @@ class TestDiscovery(unittest.TestCase):
         self.assertEqual(expected_call_count, actual_call_count)
 
         # Verifying the logger message
-        mock_logger.assert_called_with("The account credentials supplied do not have 'read' access to the following stream(s): " \
+        mock_logger.assert_called_with("Unauthorized streams excluded from catalog: " \
         "audit_logs, automations, bookmarks, brands, custom_objects, custom_roles, deleted_tickets, deleted_users, " \
         "dynamic_content_items, groups, group_memberships, macros, organizations, sessions, " \
         "sharing_agreements, side_conversations_events, recipient_addresses, suspended_tickets, tags, targets, target_failures, " \
         "tickets, ticket_audits, ticket_fields, ticket_forms, users, account_attribute_definitions, account_attributes, locales, " \
         "job_statuses, macro_actions, macro_categories, macro_definitions, monitored_twitter_handles, organization_memberships, " \
         "organization_subscriptions, support_requests, resource_collections, satisfaction_reasons, schedules, triggers, " \
-        "trigger_categories, views, workspaces, incremental_ticket_events, ticket_skips. The data for these streams would not be collected " \
-        "due to lack of required permission.")
+        "trigger_categories, views, workspaces, incremental_ticket_events, ticket_skips.")
 
     @patch("tap_zendesk.discover.LOGGER.warning")
     @patch('tap_zendesk.streams.TalkPhoneNumbers.check_access')
@@ -106,15 +105,14 @@ class TestDiscovery(unittest.TestCase):
         self.assertEqual(expected_call_count, actual_call_count)
 
         # Verifying the logger message
-        mock_logger.assert_called_with("The account credentials supplied do not have 'read' access to the following stream(s): " \
+        mock_logger.assert_called_with("Unauthorized streams excluded from catalog: " \
         "activities, audit_logs, automations, bookmarks, brands, custom_objects, custom_roles, deleted_tickets, deleted_users, " \
         "dynamic_content_items, groups, group_memberships, macros, organizations, sessions, " \
         "sharing_agreements, side_conversations_events, sla_policies, recipient_addresses, suspended_tickets, tags, " \
         "targets, target_failures, tickets, ticket_audits, ticket_fields, ticket_forms, users, account_attribute_definitions, " \
         "account_attributes, locales, job_statuses, macro_actions, macro_categories, macro_definitions, monitored_twitter_handles, " \
         "organization_memberships, organization_subscriptions, support_requests, resource_collections, satisfaction_reasons, " \
-        "schedules, triggers, trigger_categories, views, workspaces, incremental_ticket_events, ticket_skips. The data for " \
-        "these streams would not be collected due to lack of required permission.")
+        "schedules, triggers, trigger_categories, views, workspaces, incremental_ticket_events, ticket_skips.")
 
     @patch("tap_zendesk.discover.LOGGER.warning")
     @patch('tap_zendesk.streams.TalkPhoneNumbers.check_access')
@@ -147,15 +145,14 @@ class TestDiscovery(unittest.TestCase):
         self.assertEqual(expected_call_count, actual_call_count)
 
         # Verifying the logger message
-        mock_logger.assert_called_with("The account credentials supplied do not have 'read' access to the following stream(s): " \
+        mock_logger.assert_called_with("Unauthorized streams excluded from catalog: " \
         "activities, audit_logs, automations, bookmarks, brands, custom_objects, custom_roles, deleted_tickets, deleted_users, " \
         "dynamic_content_items, groups, group_memberships, macros, organizations, sessions, " \
         "sharing_agreements, side_conversations_events, recipient_addresses, suspended_tickets, tags, targets, target_failures, " \
         "tickets, ticket_audits, ticket_fields, ticket_forms, users, account_attribute_definitions, account_attributes, locales, " \
         "job_statuses, macro_actions, macro_categories, macro_definitions, monitored_twitter_handles, organization_memberships, " \
         "organization_subscriptions, support_requests, resource_collections, satisfaction_reasons, schedules, triggers, " \
-        "trigger_categories, views, workspaces, incremental_ticket_events, ticket_skips. The data for these streams would not " \
-        "be collected due to lack of required permission.")
+        "trigger_categories, views, workspaces, incremental_ticket_events, ticket_skips.")
 
     @patch('tap_zendesk.streams.TalkPhoneNumbers.check_access')
     @patch('tap_zendesk.streams.TicketMetricEvents.check_access')
